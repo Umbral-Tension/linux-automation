fc5df82dafdf2d10ceca784d7013c5ffe390ab97
umbral-tension <jdean3003@Gmail.com>
2022-09-13 21:14:59 -0500

    changed mouse_menu so that it avoids having to subclass QCommandLinkButton

--- src/mouse_app_menu.py
+++ src/mouse_app_menu.py
@@ -3,64 +3,52 @@
 import sys
 from PyQt5 import QtGui
 from PyQt5.QtWidgets import QApplication, QMainWindow, QCommandLinkButton, QBoxLayout, QWidget
-import os.path 
+from functools import partial
 import window_manager
 from jtools.jconsole import test
 
-#TODO 
-# 1. FIGURE OUT HOW TO HIDE/EXIT THE PROGRAM WHEN ONE OF THE BUTTONS IS PRESSED. MAYBE EMIT A SIGNAL OR INSTANTIATE BUTTONS WITH A PARENT WIDGET PARAMETER TO GAIN ACCESS TO THE WINDOW. 
+# TODO 
 # 2. REMOVE WINDOW BORDER
 # 3. LAUNCHING PROGRAMS FROM A SHELL WITH AN ACTIVATED VIRTUAL ENV IS HAVING SIDE EFFECTS. EX XED SAYS SOME PYTHON LIBRARIES AREN'T PRESENT. INVESTIGATE. 
-# 4. ADD EXIT LOGIC TO EXIT BUTTON
 
-
-class LauncherButton(QCommandLinkButton):
-    def __init__(self, name, parent_window):
-        super().__init__(name)
-        self.name = name
-        self.parent_window = parent_window
-
-    def mousePressEvent(self, e: QtGui.QMouseEvent) -> None:
-        self.parent_window.button_pressed(self.name)
-        return super().mousePressEvent(e)
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        # Window properties
-
 
         # make launcher buttons
         self.programs = [
-            'Firefox', 'Joplin', 'Rhythmbox', 'Spotify',
+            'Firefox', 'Joplin', 'Rhythmbox', 'Spotify',  
         ]
         self.websites = [
             'AudiobookBay', 'Artstation', 'Github', 'Gmail', 'Lichess', 'R', 'RNSFW', 'RSFW', 'Reddit', 'RedditSave', 'Youtube'
         ]
         self.buttons = {}
         for name in (self.programs + self.websites): 
-            self.buttons[name] = LauncherButton(name, self)
-        spacer_button = LauncherButton('---------------', self)
-        exit_button = LauncherButton('Exit', self)
-        self.buttons['spacer'] = spacer_button
+            self.buttons[name] = QCommandLinkButton(name)
+        spacer_button = QCommandLinkButton('---------------')
+        exit_button = QCommandLinkButton('Exit')
+        self.buttons['Spacer'] = spacer_button
         self.buttons['Exit'] = exit_button
+        # use partial from functools to connect clicked signal to a function that has that button's name baked into it. 
+        for button in self.buttons.values():
+                button.clicked.connect(partial(self.button_pressed, button.text())) 
 
         # make layout
         button_layout = QBoxLayout(QBoxLayout.Direction.TopToBottom)
         for name in sorted(self.websites):
             button_layout.addWidget(self.buttons[name])
-        button_layout.addWidget(self.buttons['spacer'])
+        button_layout.addWidget(self.buttons['Spacer'])
         for name in sorted(self.programs):
             button_layout.addWidget(self.buttons[name])
         button_layout.addWidget(self.buttons['Exit'])
         
         self.button_container = QWidget()
         self.button_container.setLayout(button_layout)
-        
         self.setCentralWidget(self.button_container)
 
     def button_pressed(self, name):
-        if name.casefold() != 'exit':
+        if name.casefold() not in ['exit', 'spacer']:
             window_manager.open(name)
         QApplication.closeAllWindows()
         
