905e07a811a8954f193c58b87a1043fa3fef255a
Jeremy Dean <jdean20@proton.me>
2023-01-15 00:59:30 -0600

    name refactorings

--- src/jipython.py
+++ src/jipython.py
@@ -1,9 +1,13 @@
 #!/bin/python3
 import os
+from os import path as opath
+from jtools.jconsole import test, ptest, zen
 from math import sin, asin, cos, acos, tan, atan, sqrt, pow, pi
 
 
 math_funcs = ['sin/asin', 'cos/acos', 'tan/atan', 'sqrt', 'pow', 'pi' ]
 
 os.system('clear')
+print('imported from os:\n\tos, os.path as opath')
+print('imported from jtools.jconsole:\n\t test, ptest, zen')
 print(f'imported from math: \n\t{math_funcs}')
