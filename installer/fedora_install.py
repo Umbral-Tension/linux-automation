c4a8d5da9acc7ac608cf57c4b9ea1f153fe2b8e4
jeremy <>
2023-05-06 16:27:42 -0500

    variable name change in installer script.

--- installer/fedora_install.py
+++ installer/fedora_install.py
@@ -40,7 +40,7 @@ if __name__ == '__main__':
     sys.path.append(f'{installerdir}/localjtools/src/')
     from jtools import jconsole as jc
     from jtools.shelldo import Shelldo
-    inst = Shelldo()
+    shelldo = Shelldo()
 
 
 
@@ -50,59 +50,59 @@ if __name__ == '__main__':
 
 
     #### install repos
-    inst.set_action('install some repositories: rpm fusion free and non-free, ')
+    shelldo.set_action('install some repositories: rpm fusion free and non-free, ')
     fedora_version = run(lex('rpm -E %fedora'), capture_output=True, text=True).stdout.strip()
-    outcome = inst.chain([
+    outcome = shelldo.chain([
         f'sudo dnf -y install "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-{fedora_version}.noarch.rpm"',
         f'sudo dnf -y install "https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-{fedora_version}.noarch.rpm"',
     ])
-    inst.log(outcome, inst.curraction)
-    inst.set_result(outcome)
+    shelldo.log(outcome, shelldo.curraction)
+    shelldo.set_result(outcome)
     
 
 
     #### install packages
-    inst.set_action('install some software: ffmpeg, multimedia codecs, mesa drivers')
-    outcome = inst.chain([
+    shelldo.set_action('install some software: ffmpeg, multimedia codecs, mesa drivers')
+    outcome = shelldo.chain([
         'sudo dnf -y swap ffmpeg-free ffmpeg --allowerasing',
         'sudo dnf -y groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin',
         'sudo dnf -y groupupdate sound-and-video',
         'sudo dnf -y swap mesa-va-drivers mesa-va-drivers-freeworld',
         'sudo dnf -y swap mesa-vdpau-drivers mesa-vdpau-drivers-freeworld',
     ])
-    inst.log(outcome, inst.curraction)
-    inst.set_result(outcome)
+    shelldo.log(outcome, shelldo.curraction)
+    shelldo.set_result(outcome)
 
 
     #### gcc
-    inst.set_action('get gcc')
-    outcome = inst.chain([inst.inst_cmd('gcc')])
-    inst.log(outcome, inst.curraction)
-    inst.set_result(outcome)
+    shelldo.set_action('get gcc')
+    outcome = shelldo.chain([shelldo.inst_cmd('gcc')])
+    shelldo.log(outcome, shelldo.curraction)
+    shelldo.set_result(outcome)
 
     
 
     #### misc stuff
-    inst.set_action('hostname')
-    outcome = inst.chain([f'hostnamectl set-hostname {hostname}'])
-    inst.log(outcome, inst.curraction)
-    inst.set_result(outcome)
+    shelldo.set_action('hostname')
+    outcome = shelldo.chain([f'hostnamectl set-hostname {hostname}'])
+    shelldo.log(outcome, shelldo.curraction)
+    shelldo.set_result(outcome)
 
 
 
     #### make ssh keys configure sshd 
-    inst.set_action('generate SSH keys and configure sshd')
+    shelldo.set_action('generate SSH keys and configure sshd')
     if not opath.exists(f'{home}/.ssh/id_ed25519'): 
-        outcome = inst.chain([f'ssh-keygen -N "" -t ed25519 -f {home}/.ssh/id_ed25519'])
-    outcome = outcome and inst.chain([f'sudo cp {appdir}/resources/configs/sshd_config /etc/ssh/sshd_config'])
-    inst.log(outcome, inst.curraction)
-    inst.set_result(outcome)
+        outcome = shelldo.chain([f'ssh-keygen -N "" -t ed25519 -f {home}/.ssh/id_ed25519'])
+    outcome = outcome and shelldo.chain([f'sudo cp {appdir}/resources/configs/sshd_config /etc/ssh/sshd_config'])
+    shelldo.log(outcome, shelldo.curraction)
+    shelldo.set_result(outcome)
     
 
 
     #### install Github client and add ssh keys to github        
-    inst.set_action('install github cli and add ssh to github')
-    outcome = inst.chain([
+    shelldo.set_action('install github cli and add ssh to github')
+    outcome = shelldo.chain([
         'sudo dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo',
         'sudo dnf -y install gh'
     ])
@@ -111,27 +111,27 @@ if __name__ == '__main__':
         a = run(lex('gh auth login -p https -w -s admin:public_key')).returncode
         b = run(lex(f'gh ssh-key add {home}/.ssh/id_ed25519.pub --title "{hostname}"')).returncode
     outcome = outcome and (a + b == 0)
-    inst.log(outcome, inst.curraction)
-    inst.set_result(outcome)
+    shelldo.log(outcome, shelldo.curraction)
+    shelldo.set_result(outcome)
     
 
 
     #### clone my usual repos into git-repos/ 
-    inst.set_action('clone my repos from github')
+    shelldo.set_action('clone my repos from github')
     repos = ['python-jtools', 'linux-automation', 'Croon', 'old-code-archive',
              'experiments', 'project-euler', 'misc-db-files']
     clone_cmds = [f'git clone git@github.com:umbral-tension/{x} {git_repos}/{x}' for x in repos]
-    outcome = inst.chain(clone_cmds, ignore_exit_code=True)
-    inst.log(outcome, inst.curraction)
-    inst.set_result(outcome)
+    outcome = shelldo.chain(clone_cmds, ignore_exit_code=True)
+    shelldo.log(outcome, shelldo.curraction)
+    shelldo.set_result(outcome)
 
 
 
     #### keyd
-    inst.set_action('download and install keyd')
-    inst.chain([f'git clone https://github.com/rvaiya/keyd {installerdir}/keyd'])
+    shelldo.set_action('download and install keyd')
+    shelldo.chain([f'git clone https://github.com/rvaiya/keyd {installerdir}/keyd'])
     os.chdir(f'{installerdir}/keyd')
-    outcome = inst.chain([
+    outcome = shelldo.chain([
         'make',
         'sudo make install',
         'sudo systemctl enable keyd',
@@ -152,19 +152,19 @@ if __name__ == '__main__':
         lines = ['[ids]\n'] + device_ids + keydfile.readlines()
         tempfile.writelines(lines)
     
-    outcome2 = inst.chain([
+    outcome2 = shelldo.chain([
         f'sudo cp {temp_conf} /etc/keyd/default.conf',
         'sudo systemctl restart keyd',
     ])
     os.remove(temp_conf)
     outcome = outcome and outcome2
-    inst.log(outcome, inst.curraction)
-    inst.set_result(outcome)
+    shelldo.log(outcome, shelldo.curraction)
+    shelldo.set_result(outcome)
 
 
 
     #### bashrc, jrouter, dconf
-    inst.set_action('bashrc, jrouter, dconf')
+    shelldo.set_action('bashrc, jrouter, dconf')
     with open(f'{home}/.bashrc', 'a') as f:
         f.writelines([f'. "{appdir}/resources/configs/bashrc fedora"\n'])
     
@@ -175,22 +175,22 @@ if __name__ == '__main__':
     os.makedirs('/home/jeremy/bin', exist_ok=True)
     os.symlink(f'{appdir}/src/linux_automation/jrouter.py', '/home/jeremy/bin/jrouter')         
     os.system(f'dconf load -f /org/gnome/settings-daemon/plugins/media-keys/ < "{appdir}/resources/dconf/dconf fedora/dirs/:org:gnome:settings-daemon:plugins:media-keys:"')
-    inst.log(True, inst.curraction)
-    inst.set_result(True)
+    shelldo.log(True, shelldo.curraction)
+    shelldo.set_result(True)
 
 
 
     #### cleanup
-    inst.set_action('cleanup')
-    outcome = inst.chain([
+    shelldo.set_action('cleanup')
+    outcome = shelldo.chain([
         f'rm -rf {installerdir}/keyd',
         f'rm -rf {installerdir}/localjtools',
-        #inst.inst_cmd('gh', uninstall=True),
+        #shelldo.inst_cmd('gh', uninstall=True),
     ])
-    inst.log(outcome, inst.curraction)
-    inst.set_result(outcome)
+    shelldo.log(outcome, shelldo.curraction)
+    shelldo.set_result(outcome)
 
 
 
     # Show final report
-    inst.report()
+    shelldo.report()
