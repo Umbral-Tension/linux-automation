78c85696527759d751561f83e37b095aa1e722c7
jeremy <>
2024-07-20 15:30:17 -0500

    added lemm.ee path and menu entry

--- src/linux_automation/mouse_app_menu.py
+++ src/linux_automation/mouse_app_menu.py
@@ -21,7 +21,7 @@ class MainWindow(QMainWindow):
             'Firefox', 'Obsidian', 'Rhythmbox', 'Spotify',  
         ]
         self.websites = [
-            'AudiobookBay', 'Artstation', 'Github', 'Lichess', 'Proton','RSFW', 'Reddit', 'RedditSave', 'Youtube'
+            'AudiobookBay', 'Artstation', 'Github', 'Lichess', 'Lemm.ee', 'Proton','RSFW', 'Reddit', 'RedditSave', 'Youtube'
         ]
         self.buttons = {}
         for name in (self.programs + self.websites): 
