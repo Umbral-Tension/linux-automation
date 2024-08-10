6c74b1ccd9563d9ace251f578ab512d8240c071c
jeremy <>
2024-08-09 22:24:47 -0500

    worked on rsync backup rewrite

--- src/linux_automation/rsync_incremental_backup.py
+++ src/linux_automation/rsync_incremental_backup.py
@@ -29,6 +29,7 @@ import argparse
 from datetime import datetime
 import subprocess
 import shlex
+import shutil
 from jtools.jconsole import test
 
 
@@ -42,7 +43,7 @@ parser.add_argument('--path-to-backups', default="/media/jeremy/internal_6TB/rsy
 if __name__ == '__main__':
 
     # args = parser.parse_args()
-    args = parser.parse_args("--dry-run".split(" "))
+    args = parser.parse_args("--default".split(" "))
     if not (args.dry_run or args.full or args.default):
         print("error: specify one of [--default, --full, --dry-run]")
         sys.exit()
@@ -56,46 +57,69 @@ if __name__ == '__main__':
 
     # Directories to backup. 
     # These MUST NOT end with a slash or rsync will copy only their contents rather than the directory AND its contents.
-    src=('"/home/jeremy/jdata"',
+    src=['"/home/jeremy/jdata"',
          '"/home/jeremy/Desktop"',
          '"/home/jeremy/jdata/jvault"',
-         '"/home/jeremy/Downloads"')
+         '"/home/jeremy/Downloads"']
 
     # dirs/files to exclude from the backup. These are given as rsync "filter rules"
     # note: Don't understand why, but ther   dir to filter must be given as how it will appear in the Destination, not how it appears locally. I.E. jdata/git-repos instead of /home/jeremy/jdata/git-repos
-    exclusions=('--exclude="jdata/git-repos"',          # syncing issues
+    exclusions=['--exclude="jdata/git-repos"',          # syncing issues
                 '--exclude="jdata/jvault"',             # --^
                 '--exclude="jdata/videos/tv"',          # already stored on backup drive
                 '--exclude="jdata/videos/movies"',      # --^
-                '--exclude="jdata/$RECYCLE.BIN"',)      # dumb Windows dir that gets added every time I boot to windows. 
+                '--exclude="jdata/$RECYCLE.BIN"',]      # dumb Windows dir that gets added every time I boot to windows. 
     
     # directory to put backups into (Default is set in arg parser) 
     backups=args.path_to_backups 
     os.makedirs(backups, exist_ok=True)
     # paths to the current latest backup and the new backup being created
-    current = opath.join(backups, "current")
-    if not opath.exists(current) or args.full:
-        newbackup = f'{backups}/{timestamp}_(Full)'
-    else:
-        newbackup = f'{backups}/{timestamp}_(Incremental)'
+    current = f'{backups}/current'
+    if not opath.exists(current):
+        args.full = True # can't do incremental if current doesn't exist
+    newbackup = f'{backups}/{timestamp}_(Full)' if args.full else f'{backups}/{timestamp}_(Incremental)'
+
     
 
 
     # Build and run an rsync command to create a new backup. Send stdout and stderr to file.
-    rs_opts = []
-    rs_opts.extend(f'--debug=FILTER --itemize-changes --human-readable --progress --archive --delete --partial --log-file="{opath.join(backups, "temp rsync log")}"'.split(" ")) 
+    rs_opts = ['--debug=FILTER', '--itemize-changes', '--human-readable', '--progress', '--archive', '--delete', '--partial', f'--log-file="{backups}/temp rsync log"']
     if not args.full: # don't do incremental if full option is given
         rs_opts.append(f'--link-dest="{current}"')
     if args.dry_run:
         rs_opts.append('--dry-run')
     rs_opts.extend(exclusions)
     
-    
-    print("running rsync as follows (unquoted spaces here are not actually passed to rsync that way): ") 
     rsync_cmd = f'rsync {" ".join(rs_opts)} {" ".join(src)} "{newbackup}" 2> "{backups}/temp rsync stderr" | tee "{backups}/temp rsync stdout"'
-    test(shlex.split(rsync_cmd))
+    print(f"running rsync as follows:\n{rsync_cmd}\n") 
+    rproc = subprocess.run(rsync_cmd, shell=True)
+
+    if args.dry_run:
+        print("\n\n\n////////// Jeremy's incremental rsync backup  ////////////")
+        print("DRY-RUN finished.")
+        shutil.move(f"{backups}/temp rsync stderr", f"{backups}/{timestamp} DRY-RUN rsync stderr")
+        shutil.move(f"{backups}/temp rsync stdout", f"{backups}/{timestamp} DRY-RUN rsync stdout")
+        shutil.move(f"{backups}/temp rsync log", f"{backups}/{timestamp} DRY-RUN rsync log")
+     
+    print(f"\n////////////\n//////////rsync process finished with exit code: {rproc.returncode}")
+    
+    # gather logging info 
+    # prepare a diff file with the first part of paths truncated up to the timestamp
+    if not args.full:
+        mydiff=f"{newbackup}/DIFF CHANGES SINCE PREVIOUS INCREMENTAL BACKUP"
+        print('running diff...')
+        diff_cmd = f'diff -rq --exclude="DIFF CHANGES SINCE PREVIOUS INCREMENTAL BACKUP" --exclude="rsync log" --exclude="rsync stdout" --exclude="rsync stderr" \
+        --exclude="ADDITIONS" --exclude="DELETIONS" "{newbackup}" "{opath.realpath(current)}"'# | sort > "{mydiff}"'
+        print(diff_cmd)
+        diffproc = subprocess.run(diff_cmd, shell=True)
+        
+        
+        subprocess.run(f'sed -i -e "s:{backups}/::g" "{mydiff}"')
+        print(f"\nfinished diff ({diffproc.returncode})")
+        
     
 
+    # test(rproc)
 
 
 
