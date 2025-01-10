a5cb271e44dba1b95e9c34847f9bbeca2352eb40
jeremy <>
2025-01-09 22:16:18 -0500

    updated lossless sync script to check for files that only exist in lossy side and improved console formatting.

--- src/linux_automation/audio_file_management/sync_lossless_to_lossy.py
+++ src/linux_automation/audio_file_management/sync_lossless_to_lossy.py
@@ -1,21 +1,25 @@
 #!/bin/python3
-""" Script to make and update a lossy mirror of my lossless music collection. Does not mirror deletions or file changes (eg tag edits) that occur in the lossless collection. """
+""" Script to make and update a lossy mirror of my lossless music collection. 
+
+- Does not mirror deletions or file changes (eg tag edits) that occur in the lossless collection. 
+- Does warn of files that only exist in the lossy mirror (perhaps because I mistakenly placed a newly acquired file into the lossy rather than lossless directory)
+"""
 
 import os, shutil
 from os import path as opath
 from os.path import join as pjoin
 import subprocess, shlex
-from jtools.jconsole import test, ptest
+from jtools import jconsole
 
 root_lossless = "/run/media/jeremy/internal_6TB/lossless_music/"
 root_lossy = "/home/jeremy/jdata/audio/music/"
 
 os.makedirs(root_lossy, exist_ok=True)
-
+newarrivals = []
 for dirpath, dirnames, filenames in os.walk(root_lossless):
+    relpath = dirpath.replace(root_lossless, "")
     for x in filenames:
         name, ext = opath.splitext(x)
-        relpath = dirpath.replace(root_lossless, "")
         isflac = ext.casefold() == ".flac"
         
         if isflac:
@@ -23,7 +27,7 @@ for dirpath, dirnames, filenames in os.walk(root_lossless):
         else:
             lossypath = pjoin(root_lossy, relpath, x)
 
-        # If mirror doesn't already have this file
+        # If mirror doesn't already have this file, then do...
         if not opath.exists(lossypath):
             os.makedirs(pjoin(root_lossy, relpath), exist_ok=True)
             if isflac:
@@ -45,6 +49,34 @@ for dirpath, dirnames, filenames in os.walk(root_lossless):
                 #just copy it to mirror
                 print(f"copying: {pjoin(dirpath, x)}")
                 shutil.copy(pjoin(dirpath, x), lossypath)
+            
+            jconsole.erase_line_previous()
+            newarrivals.append(f"{relpath}/{name}")
+
+
+# Check for files that only exist in the lossy mirror
+lossyuniques = []
+for dirpath, dirnames, filenames in os.walk(root_lossy):
+    lossydirpath = dirpath
+    losslessdirpath = dirpath.replace(root_lossy, root_lossless)
+    for x in [opath.splitext(j) for j in os.listdir(lossydirpath)]:
+        try:
+            losslessfiles = [opath.splitext(x)[0] for x in os.listdir(losslessdirpath)]
+        except FileNotFoundError:
+            lossyuniques.extend([opath.join(lossydirpath, t) for t in os.listdir(lossydirpath)])
+            break
+        if x[0] not in losslessfiles:
+            lossyuniques.append(opath.join(lossydirpath, x[0] + x[1]))
+
+print(f"Files synced from lossless to lossy:\n\t", end="")
+if len(newarrivals) == 0:
+    print(jconsole.red("up to date; nothing to sync"))
+else:
+    print(jconsole.green("\n\t".join(sorted(newarrivals))))
+if len(lossyuniques) > 0:
+    print(f"These files only exist in the lossy directory (investigate):\n\t", end="")
+    print(jconsole.red("\n\t".join(sorted(lossyuniques))))
+            
 
 
         
